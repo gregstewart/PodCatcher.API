f9e4dedfd5ba30e1de890d54e3e3b2520daa5fa1
Greg Stewart <gregs@tcias.co.uk>
2014-02-05 18:04:39 +0000

    Removed unused references

--- PodCatcher.API.Tests/Models/PodcastRepository.cs
+++ PodCatcher.API.Tests/Models/PodcastRepository.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using NUnit.Framework;
 using PodCatcher.API.Models;
 using PodCatcher.API.Tests.Stubs;
