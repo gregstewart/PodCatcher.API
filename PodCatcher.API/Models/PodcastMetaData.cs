47d83f8e5a3dec32bf035905b6f2ae966cba27a6
Greg Stewart <gregs@tcias.co.uk>
2014-04-29 23:06:04 -0700

    Somehow missed that the new PodcastMetaData file was not added

---
+++ PodCatcher.API/Models/PodcastMetaData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using PodCatcher.API.Models.Podcasts;
+
+namespace PodCatcher.API.Models
+{
+    public class PodcastMetaData : MetaData
+    {
+        private Podcast podcast;
+        
+        public PodcastMetaData(Podcast podcast, Uri entryPointUri)
+        {
+            this.podcast = podcast;
+            this.entryPointUri = entryPointUri;
+        }
+
+        public PodcastMetaData(Uri entryPointUri, String path)
+        {
+            this.entryPointUri = entryPointUri;
+            this.path = path;
+        }
+
+        public Uri SubscribeLink
+        {
+            get
+            {
+                var constructedUri = FixTrailingSlash(this.entryPointUri.ToString());
+                constructedUri += this.path + "/subscribe";
+                return new Uri(constructedUri);
+            }
+        }
+
+    }
+}
